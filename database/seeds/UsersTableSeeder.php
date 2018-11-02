901fef86cd37c47338b3750947ade2b522e2e92e
Divine <tetteydivine0@gmail.com>
2018-11-02 18:33:44 +0000

    setting up users migrations

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'=> 'Divine Tettey', 
+            'email'=>'tetteydivine0@gmail.com',
+            'password'=> 'someguydumb',
+            'email_verified_at'=> Carbon::now()
+            ]);
+    }
+}
